2b7e245e37e9afb8f574827e68377aaf8fb98bac
NullStackDev <NSD@147342.user.farlep.net>
2016-08-24 00:45:38 +0300

    Initial Commit

---
+++ SmartGitClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartGitClient
+//
+//  Created by NullStackDev on 24.08.16.
+//  Copyright © 2016 NullStackDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
